162d5b1acb2ab84f2694f78699b4cef511aa571c
Vitor Urdiali <vitorurdiali@gmail.com>
2020-04-14 12:28:34 -0300

    Criação do Tabuleiro e Peças

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,12 @@
 package application;
 
 import tabuleiro.Position;
+import tabuleiro.Tabuleiro;
 
 public class Program {
 	
 	public static void main(String[] args) {
 	
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 	}
 }
